c8f9c1755c903ee99427f4e5add0565ca4344ca2
cliffordkleinsr <cnjoroge925@gmail.com>
2026-04-17 11:30:33 +0300

    expose server ports

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,17 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+	plugins: [tailwindcss()],
+    preview: {
+      port: 4321,
+      host: true,    // This enables listening on all network interfaces
+    },
+    server: {        // Also add this for development server
+      host: true,    // This enables listening on all network interfaces
+      port: 4321
+    }
   },
   image: {
-    domains: ["s3.oilepo.com"],
+	  domains: ["s3.oilepo.com"],
   }
 });
